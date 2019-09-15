7dc6c13d8cc1e2445b45194edb04f5b05ab0fa97
Plante141452 <chaseplante@gmail.com>
2019-09-15 17:15:40 -0400

    Setup quotes

---
+++ PortfolioTracker.Origin/PortfolioTracker.Origin.Api/Controllers/QuotesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioTracker.Origin.AlphaClient;
+using PortfolioTracker.Origin.Common.Models;
+using PortfolioTracker.Origin.DataAccess;
+
+namespace PortfolioTracker.Origin.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private readonly AlphaClientLogic _alphaClientLogic;
+
+        public QuotesController()
+        {
+            _alphaClientLogic = new AlphaClientLogic(new AlphaClientWrapper(new ApiKeyProvider()), new StockDataAccess(new MongoClientWrapper(new ConnectionStringProvider())));
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public ActionResult<List<Quote>> Get(string symbols)
+        {
+            {
+                var quoteSymbols = symbols.Split(',').ToList();
+                return _alphaClientLogic.GetQuotes(quoteSymbols).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
