6f2de1b4066cc130dc6a09d428209c8d9f54f0d9
Chase Plante <chase_plante@trimble.com>
2019-12-03 08:16:37 -0500

    Bug correct attempt

--- PortfolioTracker/PortfolioTracker.Models/HistoryQueueItem.cs
+++ PortfolioTracker/PortfolioTracker.Models/HistoryQueueItem.cs
@@ -1,9 +1,16 @@
-﻿namespace PortfolioTracker.Models
+﻿using Newtonsoft.Json;
+
+namespace PortfolioTracker.Models
 {
     public class HistoryQueueItem
     {
+        [JsonProperty]
         public string EventName { get; set; }
+
+        [JsonProperty]
         public string Symbol { get; set; }
+
+        [JsonProperty]
         public StockHistory Data { get; set; }
     }
 }
